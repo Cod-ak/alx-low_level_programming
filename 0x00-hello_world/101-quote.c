2363632077347297c4526161519cf6abcacda206
akpeme kelly <ajcapstar@gmail.com>
2022-09-09 00:05:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,14 @@
-#includ#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-        write(STDOUT_FILENO, "and that piece of art is useful
-                        \" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+#include <stdio.h>                                                            
+#include <unistd.h>                                                           
+/**                                                                           
+ * main - entry point                                                         
+ *                                                                            
+ * Description: prints and that piece..                                       
+ *Return: 1 if sucessful                                                      
+ */                                                                           
+int main(void)                                                                
+{                                                                             
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
+n", 59);                                                                      
+        return (1);                                                           
 }
-e <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-        write(STDOUT_FILENO, "and that piece of art is useful
--- INSERT (paste) --       
