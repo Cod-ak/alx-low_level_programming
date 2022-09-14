26bdb7f05fb877fce721d84dcc08719e123dde55
akpeme kelly <ajcapstar@gmail.com>
2022-09-13 20:01:50 -0700

    #ifndef HOLBERTON_H_INCLUDED

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,9 +9,11 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
+void jack_baver(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_times_tables(int n);
 void num(int r, int n);
+
+#endif
